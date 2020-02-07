c039086c2ebb20e7f70869e62239a19276abfe74
Eugene Rwagasore <eugene.rwagasore@fao.org>
2020-02-07 16:43:45 +0100

    Committed previously ignored cutom.el

---
+++ custom.el
@@ -0,0 +1,22 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#212121" :foreground "#eeffff"))))
+ '(font-lock-constant-face ((t (:foreground "#C792EA"))))
+ '(font-lock-keyword-face ((t (:foreground "#2BA3FF" :slant italic))))
+ '(font-lock-preprocessor-face ((t (:inherit bold :foreground "#2BA3FF" :slant italic :weight normal))))
+ '(font-lock-string-face ((t (:foreground "#C3E88D"))))
+ '(font-lock-type-face ((t (:foreground "#FFCB6B"))))
+ '(font-lock-variable-name-face ((t (:foreground "#FF5370"))))
+ '(mode-line ((t (:background "#191919" :box nil))))
+ '(mode-line-inactive ((t (:background "#282828" :foreground "#5B6268" :box nil))))
+ '(term ((t (:foreground "#fafafa")))))
+
+;;; custom.el ends here
+
